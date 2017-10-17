c1c8b1a2c487bf57d1249c16efe56e62df4d2070
shruthi48 <shruthiprakash48@gmail.com>
2017-10-17 16:02:29 +0100

    session-2 - html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'Webpack Complete Guide',
+            template: './src/index.html',
+            minify: {
+                collapseInlineTagWhitespace: true,
+                collapseWhitespace: true
+            },
+            hash: true
+        })
+    ]
 }
